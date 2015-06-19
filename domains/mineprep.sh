420a8ee69b5f41e5b2a80b1710e77ba89fa320c7
k8hertweck <k8hertweck@gmail.com>
2015-06-19 18:13:31 -0500

    generalizing scripts

--- domains/mineprep.sh
+++ domains/mineprep.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-##Prep input fasta file (assemblies from other people) and run RepeatMasker
-##assembly files listed in assembly.lst
-##dependencies: 
+## Prep input fasta file (assemblies from other people) and run RepeatMasker
+## assembly files listed in TAXONAssembly.lst
+## usage: ./mineprep.sh TAXON
+## dependencies: 
 #	samtools
 #	repeatmasker
 
-AGAVE=~/Copy/Agave/assemblies
-REPEATMASKER=~XXX
+ASSEMBLY=~/Copy/$1/assembly
+RESULTS=~/Copy/$1/results
 
-cd $AGAVE
+cd $ASSEMBLY
 
-for x in `cat assembly.lst`
+for x in `cat $RESULTS/$1Assembly.lst`
 	do 
-		mkdir ../results/$x ../results/$x/contig	  			#setup directory
-		sed 's/n/N/' $x.fas > ../results/$x/contig/contig.fas 	#remove lowercase ns from data
-		cd ../results/$x/contig 								#move to directory
-		samtools faidx $x.fas 									#index assembly file
-		$REPEATMASKER -species liliopsida $x.fas 				#run repeatmasker
-		cd $AGAVE						
+		mkdir $RESULTS/$x $RESULTS/$x/contig		  			#setup directory
+		sed 's/n/N/' $x/$x.fas > $RESULTS/$x/contig/contig.fas 	#remove lowercase ns from data
+		cd $RESULTS/$x/contig 									#move to directory
+		samtools faidx contig.fas 								#index assembly file
+		#$REPEATMASKER -species liliopsida $x.fas 				#run repeatmasker
+		cd $ASSEMBLY						
 done
  
