e14a5df102b4272dd60d70fe5a29066efc871329
k8hertweck <k8hertweck@gmail.com>
2015-06-22 15:01:18 -0500

    mods for only assembled reads

--- pclouds/pclouds.sh
+++ pclouds/pclouds.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 ### P-clouds repeat analysis
+## estimates repeat proportion for assembled contigs
 ## dependencies:
 ##	pclouds (http://www.evolutionarygenomics.com/ProgramsData/PClouds/PClouds.html)
-##	fastx toolkit (http://hannonlab.cshl.edu/fastx_toolkit/index.html)
 
 PCLOUD=/Applications/P-Clouds_v0.9d
 
-## fastq to fasta conversion (keeping sequences with Ns)
-fastq_to_fasta -n -i INFILE -o OUTFILE
-
 ## pre-processing in pclouds
 $PCLOUD/preprocessor INFILE OUTFILE
+
+## run pclouds
