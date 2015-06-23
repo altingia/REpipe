b7134704633fed2222f88e5116a9be3cc190d800
k8hertweck <k8hertweck@gmail.com>
2015-06-23 18:13:13 -0500

    generalizing INT.sh and updating new filtering

--- domains/INT.sh
+++ domains/INT.sh
@@ -4,18 +4,19 @@
 ## usage: ./INT.sh TAXON
 ## taxa to be analyzed in TAXONINT.lst
 ## dependencies: 
-#	blast+ (makeprofiledb, rpstblastn)
+#	blast+: rpstblastn and makeprofiledb
 #	samtools
 #	cd-hit-est
 
 RESULTS=~/Copy/$1/results
-DOMAIN=~/Copy/REdata/referenceSeq/domains
+DOMAIN=~/data/domains/CDD/cdd
+
+## make custom rps-blast library
+#cd $DOMAIN
+#makeprofiledb -in INT.pn -out INT
 
 cd $RESULTS
 echo INTEGRASE > $RESULTS/$1INT.out
-
-## make custom rps-blast library
-makeprofiledb -in $DOMAIN/custom/INT.lst
 
 for x in agave_deserti #`cat $1INT.lst`
 	do
@@ -25,25 +26,30 @@ for x in agave_deserti #`cat $1INT.lst`
 		cd INT
 		
 		### Run rps-blast
-		rpstblastn -query ../contig/contig.fas -db $DOMAIN/custom/INT.lst -out INTrpsblast.out -evalue 0.01 -outfmt 7 -max_target_seqs 1
+		rpstblastn -query ../contig/contig.fas -db $DOMAIN/INT -out INTrpsblast.out -evalue 0.01 -outfmt 7 -max_target_seqs 1
 		
-		### Simplify rps-blast output and filter results shorter than 120
+		### Simplify rps-blast output and filter results
 		echo $x | tee -a $RESULTS/$1INT.out
+		## extract positive hits
 		grep "gnl" INTrpsblast.out > INTall.out
 		wc -l INTall.out | tee -a $RESULTS/$1INT.out
-		awk '{if ($4 > 120) print $0}' INTall.out > INT.out
-		wc -l INT.out | tee -a $RESULTS/$1INT.out
-		cut -f 1 INT.out > INT.lst
+		## filter out hits less than 120 aa in length and print ranges of hits to pass to samtools
+		awk '{if ($4 > 120) print $0}' > temp
+		## put ranges in numerical order
+		#":"$7"-"$8}' INTall.out > INT.lst
+		wc -l INT.lst | tee -a $RESULTS/$1INT.out
+		rm temp
 
-		##pull out fasta
+
+		## pull out fasta
 		samtools faidx ../contig/contig.fas $(cat INT.lst) > INT.fas
 		
-		##align and correct for directionality
+		## align and correct for directionality
 		mafft --adjustdirectionaccurately INT.fas > INT.afa
 		
-		#cluster results to remove redundancy
-		cd-hit-est -i INT.afa -o INT -c 0.9 -n 8 -aL 0.9 -aS 0.9 -g 1
-		wc -l INT | tee $RESULTS/$1INT.out
+		## cluster results to remove redundancy
+		cd-hit-est -i INT.afa -o INT.out -c 0.9 -n 8 -aL 0.9 -aS 0.9 -g 1
+		wc -l INT.out | tee -a $RESULTS/$1INT.out
 		cd $RESULTS
 done
 		
