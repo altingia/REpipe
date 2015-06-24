28d6ae26f68c4aad718967e12ecfa3414ed4e332
k8hertweck <k8hertweck@gmail.com>
2015-06-24 17:13:44 -0500

    reordering steps

--- domains/domainTree.sh
+++ domains/domainTree.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ## phylogenetic analysis of domain searching results
-## usage: ./XXX.sh TAXON DOMAIN
+## usage: ./domainTree.sh TAXON DOMAIN
 ## reference file as DOMAIN.fas in $DOMAIN
 ## dependencies: 
 #	mafft
+#	EMBOSS (transeq, seqret)
 #	raxml
 
 RESULTS=~/Copy/$1/results
@@ -13,13 +14,12 @@ DOMAIN=~/data/domains
 cd $RESULTS
 
 ## align domain as nucleotides
-mafft --auto --phylipout --inputorder $RESULTS/INTcombined.fas > $RESULTS/INTcombined.phy
+mafft --adjustdirectionaccurately $2combined.fas > $2combined.afa
 
-## add references
-cat $RESULTS/$2combined.fas $DOMAIN/$2ref.fas > $2combref.fas
-
-## translate domain to protein and realign
-
+## translate domain to protein, add references, realign
+transeq $2combined.afa $2combined.aa.afa
+cat $2combined.aa.afa $DOMAIN/$2ref.fas > $2combref.fas
+mafft --auto $2combref.fas > $2combref.afa
 
 ## build trees for both nucleotide and protein
 raxml -n $1 -f o -m GTRGAMMA -s $1.phy 
