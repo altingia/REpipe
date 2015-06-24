cfa4e692b0e25ce0e1ba097c4a4abca61b241bc0
k8hertweck <k8hertweck@gmail.com>
2015-06-24 14:08:15 -0500

    consistent paths for downstream

--- domains/domainPrep.sh
+++ domains/domainPrep.sh
@@ -12,11 +12,11 @@ RESULTS=~/Copy/$1/results
 
 cd $ASSEMBLY
 
-for x in `cat $RESULTS/$1Assembly.lst`
+for x in `cat $RESULTS/$1.lst`
 	do 
-		mkdir $RESULTS/$x $RESULTS/$x/contig		  			#setup directory
-		sed 's/n/N/' $x/$x.fas > $RESULTS/$x/contig/contig.fas 	#remove lowercase ns from data
-		cd $RESULTS/$x/contig 									#move to directory
+		mkdir $ASSEMBLY/$x/contig		 			 			#setup directory
+		sed 's/n/N/' $x/*.fas > $ASSEMBLY/$x/contig/contig.fas 	#remove lowercase ns from data
+		cd $ASSEMBLY/$x/contig 									#move to directory
 		samtools faidx contig.fas 								#index assembly file
 		#$REPEATMASKER -species liliopsida $x.fas 				#run repeatmasker
 		cd $ASSEMBLY						
