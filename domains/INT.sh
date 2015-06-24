4f65917256000b5e91d0f950212d66c79299000d
k8hertweck <k8hertweck@gmail.com>
2015-06-24 14:51:03 -0500

    generalizing and ending with compiled file

--- domains/INT.sh
+++ domains/INT.sh
@@ -2,12 +2,13 @@
 
 ## search for integrase domains from assemblies
 ## usage: ./INT.sh TAXON
-## taxa to be analyzed in TAXONINT.lst
+## taxa to be analyzed in TAXON.lst
 ## dependencies: 
 #	blast+: rpstblastn and makeprofiledb
-#	samtools
+#	samtools (index created in previous script)
 #	cd-hit-est
 
+ASSEMBLY=~/Copy/$1/assembly
 RESULTS=~/Copy/$1/results
 DOMAIN=~/data/domains/CDD/cdd
 
@@ -15,10 +16,11 @@ DOMAIN=~/data/domains/CDD/cdd
 #cd $DOMAIN
 #makeprofiledb -in INT.pn -out INT
 
-cd $RESULTS
-echo INTEGRASE > $RESULTS/$1INT.out
+cd $ASSEMBLY
+echo "INTEGRASE" > $RESULTS/$1INT.out
 
-for x in agave_deserti #`cat $1INT.lst`
+## extract integrase domains from each taxon
+for x in `cat $RESULTS/$1.lst`
 	do
 		echo $x
 		cd $x
@@ -26,30 +28,35 @@ for x in agave_deserti #`cat $1INT.lst`
 		cd INT
 		
 		### Run rps-blast
-		rpstblastn -query ../contig/contig.fas -db $DOMAIN/INT -out INTrpsblast.out -evalue 0.01 -outfmt 7 -max_target_seqs 1
+		#rpstblastn -query ../contig/contig.fas -db $DOMAIN/INT -out INTrpsblast.out -evalue 0.01 -outfmt 7 -max_target_seqs 1
 		
 		### Simplify rps-blast output and filter results
 		echo $x | tee -a $RESULTS/$1INT.out
 		## extract positive hits
 		grep "gnl" INTrpsblast.out > INTall.out
 		wc -l INTall.out | tee -a $RESULTS/$1INT.out
-		## filter out hits less than 120 aa in length and print ranges of hits to pass to samtools
-		awk '{if ($4 > 120) print $0}' > temp
-		## put ranges in numerical order
-		#":"$7"-"$8}' INTall.out > INT.lst
+		## filter out hits less than 100 aa in length 
+		awk '{if ($4 > 100) print $0}' INTall.out > INTlength.out
+		## print ranges of hits to pass to samtools
+		awk '{if ($7 < $8)
+			print $1":"$7"-"$8;
+			else 
+			print $1":"$8"-"$7}' INTlength.out > INT.lst
 		wc -l INT.lst | tee -a $RESULTS/$1INT.out
-		rm temp
-
 
 		## pull out fasta
 		samtools faidx ../contig/contig.fas $(cat INT.lst) > INT.fas
 		
-		## align and correct for directionality
-		mafft --adjustdirectionaccurately INT.fas > INT.afa
-		
 		## cluster results to remove redundancy
-		cd-hit-est -i INT.afa -o INT.out -c 0.9 -n 8 -aL 0.9 -aS 0.9 -g 1
-		wc -l INT.out | tee -a $RESULTS/$1INT.out
-		cd $RESULTS
-done
+		cd-hit-est -i INT.fas -o INTclust.out -c 0.9 -n 8 -aL 0.9 -aS 0.9 -g 1
+		echo "clusters" | tee -a $RESULTS/$1INT.out
+		grep ">" INTclust.out | wc -l | tee -a $RESULTS/$1INT.out
 		
+		## append taxon name to fasta headers
+		sed "s/>/>$x./" INTclust.out > INTclust.fas
+	
+		cd $ASSEMBLY
+done
+
+## combine integrase from different taxa with reference
+cat */INT/INTclust.fas > $RESULTS/INTcombined.fas
