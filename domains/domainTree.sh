2f66d781e29ff48bec6279378c8a67746773f7a3
k8hertweck <k8hertweck@gmail.com>
2015-06-29 16:04:41 -0500

    reminders to fix later

--- domains/domainTree.sh
+++ domains/domainTree.sh
@@ -14,12 +14,13 @@ DOMAIN=~/data/domains
 cd $RESULTS
 
 ## align domain as nucleotides
-mafft --adjustdirectionaccurately $2combined.fas > $2combined.afa
+mafft --adjustdirectionaccurately $2.combined.fas > $2.combined.afa
 
 ## translate domain to protein, add references, realign
 transeq $2combined.afa $2combined.aa.afa
 cat $2combined.aa.afa $DOMAIN/$2ref.fas > $2combref.fas
 mafft --auto $2combref.fas > $2combref.afa
+seqret 
 
 ## build trees for both nucleotide and protein
 raxml -n $1 -f o -m GTRGAMMA -s $1.phy 
