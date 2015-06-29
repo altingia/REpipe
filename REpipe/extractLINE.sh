ed8d386c0831c95393e2d11fbe08cd777038f668
k8hertweck <k8hertweck@gmail.com>
2015-06-29 15:11:02 -0500

    sending output to list

--- REpipe/extractLINE.sh
+++ REpipe/extractLINE.sh
@@ -1,34 +1,52 @@
 #!/bin/bash
-##$1 is input taxon
-##input files: $1.fa.out, $1.fa.cidx, $1.fa
-##dependencies: cdbyank, fasta_formatter, fastx_reverse_complement
 
-##LINEs
-grep 'LINE/RTE-BovB' $1.fa.out > $1LINE.out
-##filter out partial sequences (for LINEs, less than 2000 bp)
-cat $1LINE.out | awk '{
-	if ($7-$6>2000)
-		print $0;
-	}' > $1LINEfiltered.out
+## extract LINEs from RepeatMasker screens of assemblies
+## dependencies: 
+##	samtools
+##	cd-hit-est
 
-##RTE2_ZM
-##pull out LINE fasta
-cat $1LINEfiltered.out | awk '{
-	if ($9=="+" && $10=="RTE2_ZM") 
-		print $5 " " $6 " " $7;	
-	}' | cdbyank $1.fa.cidx -R | fasta_formatter | sed 's/>/>'$1'./' > $1LINE.RTE2ZM.fa
-cat $1LINEfiltered.out | awk '{
-	if ($9=="C" && $10=="RTE2_ZM")
-		print $5 " " $6 " " $7;
-	}' | cdbyank $1.fa.cidx -R | fasta_formatter | fastx_reverse_complement | sed 's/>/>'$1'./' >> $1LINE.RTE2ZM.fa
+ASSEMBLY=~/Copy/$1/assembly
+RESULTS=~/Copy/$1/results
 
-##RTE-1_PD
-##pull out LINE fasta
-cat $1LINEfiltered.out | awk '{
-	if ($9=="+" && $10=="RTE-1_PD") 
-		print $5 " " $6 " " $7;	
-	}' | cdbyank $1.fa.cidx -R | fasta_formatter | sed 's/>/>'$1'./' > $1LINE.RTE1PD.fa
-cat $1LINEfiltered.out | awk '{
-	if ($9=="C" && $10=="RTE-1_PD")
-		print $5 " " $6 " " $7;
-	}' | cdbyank $1.fa.cidx -R | fasta_formatter | fastx_reverse_complement | sed 's/>/>'$1'./' >> $1LINE.RTE1PD.fa
+mkdir $RESULTS/LINE
+echo "LINEs" | tee $RESULTS/LINE/$1LINE.out
+
+cd $ASSEMBLY
+
+## loop across all taxa
+for x in `cat $RESULTS/$1.lst`
+	do
+		cd $x/LINE
+		## create list of unique LINEs recognized
+		awk '{print $10}' LINE.out | sort | uniq > LINEtypes.lst
+		echo $x | tee -a $RESULTS/LINE/$1LINE.out
+		wc -l LINEtypes.lst | tee -a $RESULTS/LINE/$1LINE.out
+		
+		## loop across all LINE types
+		for type in `cat LINEtypes.lst`
+			do
+				## print ranges to pass to samtools
+				grep $type LINE.out | awk '{print $5":"$6"-"$7}' > $type.lst
+				wc -l $type.lst | tee -a $RESULTS/LINE/$1LINE.out
+				## extract from fasta
+				samtools faidx ../contig/contig.fas $(cat $type.lst) s/:/./ > $type.fas
+				## cluster results to remove redundancy
+				cd-hit-est -i $type.fas -o $type.clust.out -c 0.9 -n 8 -aL 0.9 -aS 0.9 -g 1
+				grep ">" $type.clust.out | wc -l | tee -a $RESULTS/LINE/$1LINE.out
+				## append taxon name to fasta headers
+				sed "s/>/>$x./" $type.clust.out > $type.clust.fas
+		done
+		
+		cd $ASSEMBLY
+
+done	
+
+## combine LINEs from all taxa by type
+cat */LINE/LINEtypes.lst > $RESULTS/LINE/LINEall.lst
+
+for group in `cat $RESULTS/LINE/LINEall.lst`
+	do
+	cat */LINE/$group.clust.fas > $RESULTS/LINE/$group.combined.fas
+	echo $group | tee -a $RESULTS/LINE/$1LINE.out
+	grep ">" $RESULTS/LINE/$group.combined.fas | wc -l | tee -a $RESULTS/LINE/$1LINE.out
+done
