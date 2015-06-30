068684b7192566dc49b1bb21016582980a2f4a25
k8hertweck <k8hertweck@gmail.com>
2015-06-30 17:54:14 -0500

    placeholder for compiling all annotated

--- results/compare.sh
+++ results/compare.sh
@@ -6,15 +6,17 @@
 ASSEMBLY=~/Copy/$1/assembly
 RESULTS=~/Copy/$1/results
 
-cd $ASSEMBLY
-
 ## loop across all taxa
 for x in `cat $RESULTS/$1.lst`
 	do
-		cd $x
-		## RTV_1 (core domain for Gypsy)
-		sed s/:.*$// RT/249567.lst | cat - LTR/Gypsy.lst | sort | uniq -c > 
+		cd $ASSEMBLY/$x
 		
-		## RTV_2 (core domain for Copia/Sireviruses)
-		sed s/:.*$// RT/254387.lst | cat - LTR/Copia.lst | sort | uniq -c
+		## count all annotated contigs
+		#cat contig/annotate.lst INT/XXX RT/XXX | sort | uniq -c > allOverlap.lst
+		
+		## RTV_1 (core domain for Gypsy) overlap with RM Gypsy
+		sed s/:.*$// RT/249567.lst | cat - LTR/Gypsy.lst | sort | uniq -c > GypsyRTV_1.lst
+		
+		## RTV_2 (core domain for Copia/Sireviruses) overlap with RM Copia
+		sed s/:.*$// RT/254387.lst | cat - LTR/Copia.lst | sort | uniq -c > CopiaRTV_2.lst
 done	
