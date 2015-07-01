25f1d50f235bf10de3888af6e7967b30ed04f3bc
k8hertweck <k8hertweck@gmail.com>
2015-07-01 16:29:46 -0500

    standardizing with other extract scripts

--- REpipe/extractNonLTR.sh
+++ REpipe/extractNonLTR.sh
@@ -21,10 +21,10 @@ for x in `cat $RESULTS/$1.lst`
 		## create list of unique elements recognized
 		awk '{print $10}' $2.out | sort | uniq > $2types.lst
 		echo $x | tee -a $RESULTS/$2/$1$2.out
-		wc -l $2types.lst | tee -a $RESULTS/$2/$1$2.out
+		wc -l $2.types.lst | tee -a $RESULTS/$2/$1$2.out
 		
 		## loop across all element types
-		for type in `cat $2types.lst`
+		for type in `cat $2.types.lst`
 			do
 				## print ranges to pass to samtools
 				grep $type $2.out | awk '{print $5":"$6"-"$7}' > $type.lst
@@ -43,7 +43,7 @@ for x in `cat $RESULTS/$1.lst`
 done	
 
 ## combine elements from all taxa by type
-cat */$2/$2types.lst > $RESULTS/$2/$2all.lst
+cat */$2/$2.types.lst > $RESULTS/$2/$2all.lst
 
 for group in `cat $RESULTS/$2/$2all.lst`
 	do
