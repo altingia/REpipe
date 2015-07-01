0b44c66e0512aea400802059fc5c8ec3616a57be
k8hertweck <k8hertweck@gmail.com>
2015-07-01 13:00:59 -0500

    fixing comments, cleaning up QC

--- REpipe/assemblyPrep.sh
+++ REpipe/assemblyPrep.sh
@@ -9,9 +9,11 @@
 
 ASSEMBLY=~/Copy/$1/assembly
 RESULTS=~/Copy/$1/results
+SCRIPTS=~/GitHub/REpipe/REpipe
 
 for x in `cat $RESULTS/$1Assembly.lst`
 	do 
+		echo $x
 		## create directory structure
 		cd $ASSEMBLY
 		mkdir $ASSEMBLY/$x/contig		 			 			#setup directory
@@ -19,30 +21,29 @@ for x in `cat $RESULTS/$1Assembly.lst`
 		## clean data
 		sed 's/n/N/' $x/*.fas > $ASSEMBLY/$x/contig/contig.fas 	#remove lowercase ns from data
 		
-		## index contigs
+		## prep files
 		cd $ASSEMBLY/$x/contig 									#move to directory
+		grep ">" contig.fas | sed 's/>//' > contig.lst			#list contigs
 		cp contig.fas nuc.fas
+		cp contig.lst nuc.lst
 		samtools faidx contig.fas 								#index assembly file
 		
 		## run repeatmasker
 		RepeatMasker -species liliopsida nuc.fas 				#run repeatmasker
 
 		## RUN PARSING SCRIPT
+		echo "parsing RM"
 		$SCRIPTS/parseRM.sh
 
 		## QC SUMMARY
+		echo "QC"
 		cd $ASSEMBLY/$x
-		echo "QC SUMMARY" | tee $x.QCout
-		echo "contigs" | tee -a $x.QCout
-		wc -l contig/contig.lst | tee -a $x.QCout
-		echo "orgcontigs" | tee -a $x.QCout
-		wc -l contig/contigCPMT.lst | tee -a $x.QCout
+		echo "contigs" | tee $OUT
+		wc -l contig/contig.lst | tee -a $OUT
 		echo "repeatcontigs" | tee -a $x.QCout
 		wc -l contig/contigRE.lst | tee -a $x.QCout
 		echo "retrotransposoncontigs" | tee -a $x.QCout
 		wc -l LTR/LTR.lst | tee -a $x.QCout
-		echo "retrotransposon.fas" | tee -a $x.QCout
-		wc -l LTR/LTR.fas | tee -a $x.QCout
 
 done
  
