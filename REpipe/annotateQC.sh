a99dcb7f476047a6902dc018893768971a5de32d
k8hertweck <k8hertweck@gmail.com>
2015-07-01 22:53:16 -0500

    path to out

--- REpipe/annotateQC.sh
+++ REpipe/annotateQC.sh
@@ -11,21 +11,21 @@ for x in `cat $1`
 	do
 
 	cd $ANNOTATE/$x/$2
-	echo $x | tee $1.QCout
-	echo "contigs" | tee -a $1.QCout
-	wc -l contig/contig.lst | tee -a $1.QCout
-	echo "mappedreads" | tee -a $1.QCout
-	awk '{s+=$3}END{print s}' contig/contigReads.lst | tee -a $1.QCout
-	echo "unmappedreads" | tee -a $1.QCout
-	awk '{s+=$4}END{print s}' contig/contigReads.lst | tee -a $1.QCout
-	echo "orgcontigs" | tee -a $1.QCout
-	wc -l contig/contigCPMT.lst | tee -a $1.QCout
-	echo "orgreads" | tee -a $1.QCout
-	grep -f contig/contigCPMT.lst contig/contigReads.lst | awk '{s+=$3}END{print s}' | tee -a $1.QCout
-	echo "repeatcontigs" | tee -a $1.QCout
-	wc -l contig/contigRE.lst | tee -a $1.QCout
-	echo "repeatreads" | tee -a $1.QCout
-	grep -f contig/contigRE.lst contig/contigReads.lst | awk '{s+=$3}END{print s}' | tee -a $1.QCout
+	echo $x | tee $2.QCout
+	echo "contigs" | tee -a $2.QCout
+	wc -l contig/contig.lst | tee -a $2.QCout
+	echo "mappedreads" | tee -a $2.QCout
+	awk '{s+=$3}END{print s}' contig/contigReads.lst | tee -a $2.QCout
+	echo "unmappedreads" | tee -a $2.QCout
+	awk '{s+=$4}END{print s}' contig/contigReads.lst | tee -a $2.QCout
+	echo "orgcontigs" | tee -a $2.QCout
+	wc -l contig/contigCPMT.lst | tee -a $2.QCout
+	echo "orgreads" | tee -a $2.QCout
+	grep -f contig/contigCPMT.lst contig/contigReads.lst | awk '{s+=$3}END{print s}' | tee -a $2.QCout
+	echo "repeatcontigs" | tee -a $2.QCout
+	wc -l contig/contigRE.lst | tee -a $2.QCout
+	echo "repeatreads" | tee -a $2.QCout
+	grep -f contig/contigRE.lst contig/contigReads.lst | awk '{s+=$3}END{print s}' | tee -a $2.QCout
 	echo "retrotransposoncontigs"
-	wc -l LTR/LTR.lst | tee -a $1.QCout
+	wc -l LTR/LTR.lst | tee -a $2.QCout
 done
