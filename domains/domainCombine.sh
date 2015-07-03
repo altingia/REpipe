60268bdd1ec51d2d5979e6e16e8e21155452b67c
k8hertweck <k8hertweck@gmail.com>
2015-07-03 17:38:27 -0500

    adding table summary

--- domains/domainCombine.sh
+++ domains/domainCombine.sh
@@ -11,8 +11,17 @@ mkdir $RESULTS/$2
 
 cd $ANNOTATE
 
+## create master fasta and recluster
 for type in `cat $DOMAIN/$2cdd.lst`
 	do
-		cat */$2/$type.clust.fas > $RESULTS/$2/$type.combined.fas
-		grep ">" $RESULTS/$2/$type.combined.fas | wc -l
+		cat */$2/$type.clust.fas > $RESULTS/$2/$type.combine.fas
+		echo $2.$type.all | tee -a $RESULTS/$2/$2.combine.out
+		grep ">" $RESULTS/$2/$type.combine.fas | wc -l | tee -a $RESULTS/$2/$2.combine.out
+		cd-hit-est -i $RESULTS/$2/$type.combine.fas -o $RESULTS/$2/$type.combine.clust.out -c 0.9 -n 8 -aL 0.9 -aS 0.9 -g 1
+		echo $2.$type.clust | tee -a $RESULTS/$2/$2.combine.out
+		grep ">" $RESULTS/$2/$type.combine.clust.out | wc -l | tee -a $RESULTS/$2/$2.combine.out
 done
+
+## reformat table
+cd $RESULTS/$2
+sed "s/^$/0/" $2.combine.out | sed -E "s/\ +//" | cut -f 1 -d " " | paste -d " " - - | sed "s/-//" > $2.combine.table
