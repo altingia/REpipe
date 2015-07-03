ca488dcda7cfadf2ea5a160fde99ca289619994e
k8hertweck <k8hertweck@gmail.com>
2015-07-03 15:22:02 -0500

    fixing directory and indentation

--- domains/domainCombine.sh
+++ domains/domainCombine.sh
@@ -3,12 +3,16 @@
 ## combine domain results for all taxa in one fasta
 ## usage: ./domainCombine.sh TAXON DOMAIN
 
+ANNOTATE=~/Copy/$1/annotate
 RESULTS=~/Copy/$1/results
 DOMAIN=~/data/domains
 
 mkdir $RESULTS/$2
+
+cd $ANNOTATE
+
 for type in `cat $DOMAIN/$2cdd.lst`
 	do
-	cat */$2/$type.clust.fas > $RESULTS/$2/$type.combined.fas
-	grep ">" $RESULTS/$2/$type.combined.fas | wc -l
+		cat */$2/$type.clust.fas > $RESULTS/$2/$type.combined.fas
+		grep ">" $RESULTS/$2/$type.combined.fas | wc -l
 done
