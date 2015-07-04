863c8449286a123e21f66e0518e4ae6902d41615
k8hertweck <k8hertweck@gmail.com>
2015-07-04 17:41:45 -0500

    cross validation between annotation methods

--- results/compare.sh
+++ results/compare.sh
@@ -11,6 +11,7 @@ for x in `cat $2`
 	do
 		cd $ANNOTATE/$x
 		mkdir compare
+		rm compare/*
 		
 		## count all annotated contigs
 		for domain in INT RT RH GAG
@@ -20,9 +21,28 @@ for x in `cat $2`
 		
 		cat contig/annotate.lst compare/*.lst | sort | uniq -c | sort -r > compare/allOverlap.lst
 		
-		## RTV_1 (core domain for Gypsy) overlap with RM Gypsy
-		sed s/:.*$// RT/249567.lst | cat - LTR/Gypsy.lst | sort | uniq -c > compare/GypsyRTV_1.lst
+		## Gypsy, Ty3
+		## RM Gypsy
+		## RTV_1 (core domain for Gypsy) 249567
+		## rve (integrase for Copia and Gypsy) 250040 
+		cat LTR/Gypsy.lst RT/249567.uniq.lst INT/250040.uniq.lst | sort | uniq -c | sort -r > compare/GypsyCompare.lst
 		
-		## RTV_2 (core domain for Copia/Sireviruses) overlap with RM Copia
-		sed s/:.*$// RT/254387.lst | cat - LTR/Copia.lst | sort | uniq -c > compare/CopiaRTV_2.lst
+		
+		## Copia, Ty1
+		## RM Copia
+		## RTV_2 (core domain for Copia/Sireviruses) 254387 
+		## rve (integrase for Copia and Gypsy) 250040 
+		cat LTR/Copia.lst RT/254387.uniq.lst INT/250040.uniq.lst | sort | uniq -c | sort -r > compare/CopiaCompare.lst
+		
+		## DIRS
+		## RT_DIRS1 239684
+		## RNAse_HI_RT_DIRS1 260007
+		cat RT/239684.uniq.lst RH/260007.uniq.lst | sort | uniq -c | sort -r > compare/DIRSCompare.lst
+		
+		## non-LTR
+		## RM LINE and SINE
+		## RNAse_HI_RT_non_LTR 260008
+		## RT_nLTR_like 238827
+		cat LINE/LINE.lst SINE/SINE.lst RH/260008.uniq.lst RT/238827.uniq.lst | sort | uniq -c | sort -r > compare/nonLTRCompare.lst
+
 done	
