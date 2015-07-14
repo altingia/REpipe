a31b707df62ff2baaf73a6f5668a149768bfbaa7
k8hertweck <k8hertweck@gmail.com>
2015-07-14 16:59:01 -0500

    summarizing pairwise clusters

--- results/clusterSummary.sh
+++ results/clusterSummary.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
 ## counts shared clusters
-## usage: ./clusterSummary.sh TAXON TAXON.LST
+## usage: ./clusterSummary.sh TAXON CLUSTER.LST
 
 RESULTS=~/Copy/$1/results
+SCRIPTS=~/GitHub/REpipe
 
-## loop across all taxa
-for x in `cat $2`
+cd $RESULTS
+
+## set up output file
+echo -n > comparisons.lst
+for y in `cat $2`
+			do
+				for z in `cat $2`
+					do
+						echo "$y $z" >> comparisons.lst
+				done
+		done
+
+## summarize cluster data
+for x in */*.combine.clust.out.clstr
 	do
-		cd $ANNOTATE/$x
-		mkdir compareTrans
-		rm compareTrans/*	
-		
-		
-done	
+		echo -n > $x.count
+		awk '{print $1" "$3}' $x | sed 's/\..*$//g' | cut -d " " -f 2 | uniq | sed s/\>// > $x.temp
+		awk '{if ($0 =="") print ""; else printf "%s ",$0}END{print "";}' $x.temp > $x.temp2 
+		for y in `cat $2`
+			do
+				for z in `cat $2`
+					do
+						echo "$y $z" >> $x.count
+						grep -c -E "$y.*$z" $x.temp2 >> $x.count
+				done
+		done
+		cat $x.count | paste -d " " - - | cut -d " " -f 3 > $x.lst
+		paste comparisons.lst $x.lst > temp
+		cp temp comparisons.lst
+		rm $x.count $x.temp* temp
+done
+
+## finalize table
+sed s/\ /,/ comparisons.lst | tr "\t" "," > domains/combineDomains.csv
